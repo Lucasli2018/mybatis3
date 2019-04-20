4cd3b2389917ac3332ce888881c78202a654cb8d
lucas <1906859953@qq.com>
2019-04-20 15:01:18 +0800

    mybatis入门

---
+++ mybatisdemo/src/main/java/com/lucas/maven/mybatisdemo/App.java
@@ -0,0 +1,13 @@
+package com.lucas.maven.mybatisdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mybatisdemo/src/main/java/com/lucas/maven/mybatisdemo/pojo/User.java
@@ -0,0 +1,45 @@
+package com.lucas.maven.mybatisdemo.pojo;
+
+public class User {
+	private int id;
+	private String username;
+	private String sex;
+	private String address;
+	private String birthday;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", sex=" + sex + ", address=" + address + ", birthday="
+				+ birthday + "]";
+	}
+	
+}
